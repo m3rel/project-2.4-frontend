0eb05ab942d4c4b436aeb7dd8f681d96f93f49f0
merel <hahahahmerel@gmail.com>
2026-05-21 15:25:08 +0200

    workflow

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/project-2.4-frontend/',  // ← just add this line!
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
