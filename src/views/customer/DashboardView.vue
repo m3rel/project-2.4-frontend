a931fbcaa26b592a5b0aaa6af360d73e54b9a8ae
Sealhond <bjpkuiper02@gmail.com>
2026-06-04 11:15:22 +0200

    added logout button to customer dashboard to not get stuck

--- src/views/customer/DashboardView.vue
+++ src/views/customer/DashboardView.vue
@@ -1,7 +1,25 @@
 <template>
   <h1 class="text-black">customer dashboardddddd</h1>
-
+  <button
+        @click="handleLogout"
+        class="p-3 rounded-lg text-sm text-gray-700 hover:bg-white/40 transition-colors text-left"
+      >
+        Logout
+      </button>
 
 
 
 </template>
+
+<script setup>
+import { useAuthStore } from '@/stores/auth.js'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+const handleLogout = () => {
+  authStore.logout()
+  router.push('/login')
+}
+</script>
